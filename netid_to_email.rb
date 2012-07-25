5cd66d16acd8c991c476783d55f760d65df303f0
Adam Bray <adam.bray@yale.edu>
2012-07-25 14:19:00 -0400

    initial import

---
+++ netid_to_email.rb
@@ -0,0 +1,28 @@
+require 'net-ldap'
+require 'csv'
+
+PATH = ARGV[0]
+LDAP_ATTRS = ["uid", "givenname", "sn", "mail", "collegename", "college", "class"]
+
+netids = CSV.read(PATH).flatten
+
+ldap = Net::LDAP.new(:host => "directory.yale.edu", :port => 389)
+
+people = []
+
+
+netids.each_with_index do |netid, i|
+	filter = Net::LDAP::Filter.eq("uid", netid)
+	result = ldap.search(:base => "ou=People,o=yale.edu", :filter => filter, :attributes => LDAP_ATTRS)
+    people[i] = []
+    LDAP_ATTRS.each do |attr|
+    	people[i] << result[0][attr.to_sym][0]
+    end   				
+end
+
+CSV.open("output.csv", "wb") do |csv|
+	csv << LDAP_ATTRS
+	people.each do |person|
+		csv << person
+	end
+end
