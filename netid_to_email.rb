07a06efa8a2436fa2feaf93c17c785e0ccb00de9
caseywatts <casey.s.watts@gmail.com>
2013-04-11 10:43:03 -0400

    code cleanup

--- netid_to_email.rb
+++ netid_to_email.rb
@@ -1,34 +1,39 @@
 require 'net-ldap'
 require 'csv'
 
+#Pull NetIDs from the first variable given to this script
 PATH = ARGV[0]
-LDAP_ATTRS = ["uid", "givenname", "sn", "mail", "collegename", "college", "class"]
-
 netids = CSV.read(PATH).flatten
 
-ldap = Net::LDAP.new(:host => "directory.yale.edu", :port => 389)
+#Search based on these LDAP attributes
+LDAP_ATTRS = ["uid", "givenname", "sn", "mail", "collegename", "college", "class"]
 
+#Misc Setup
+ldap = Net::LDAP.new(:host => "directory.yale.edu", :port => 389)
 people = []
 
+#Loop one netid at a time
 netids.each_with_index do |netid, i|
-	puts "#{i*100/netids.size}%"
-	
+  puts "#{i*100/netids.size}%"
+
+  #Search LDAP by netid
   filter = Net::LDAP::Filter.eq("uid", netid)
-	result = ldap.search(:base => "ou=People,o=yale.edu", :filter => filter, :attributes => LDAP_ATTRS)
-    people[i] = []
-  
-    if result[0]
-	    LDAP_ATTRS.each do |attr|
-	    	people[i] << result[0][attr.to_sym][0]
-	    end   				
-    else
-      people[i] << netid << "NOT VALID"
-	end
+  result = ldap.search(:base => "ou=People,o=yale.edu", :filter => filter, :attributes => LDAP_ATTRS)
+  people[i] = []
+
+  if result[0]
+    LDAP_ATTRS.each do |attr|
+      people[i] << result[0][attr.to_sym][0]
+    end   				
+  else
+    people[i] << netid << "NOT VALID"
+  end
 end
 
+#Write results to output.csv
 CSV.open("output.csv", "wb") do |csv|
-	csv << LDAP_ATTRS
-	people.each do |person|
-		csv << person
-	end
+  csv << LDAP_ATTRS
+  people.each do |person|
+    csv << person
+  end
 end
